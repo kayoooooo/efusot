ce9b354714a400cd62bf4b41c7e5fb68e5253008
Kevin Yao <k.yao.kayoo@gmail.com>
2022-04-06 14:01:53 -0400

    trying out how to use jsons by generalizing the insult command.

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { Client } = require('discord.js');
+import fs from "fs";
 const { botIntents } = require('./config/config');
 const config = require('./config/default');
 const client = new Client({
@@ -11,7 +12,8 @@ client.on('ready', () => {
 });
 const commands = {
 	funny: "oigoig",
-	reminder: "remind"
+	reminder: "remind",
+	insult: "insult"
 };
 
 const insults = ["Kill yourself", "Go fuck yourself", "Fuck you", "Eat shit"];
@@ -31,14 +33,30 @@ client.on('messageCreate', (msg) => {
 		msg.reply("775249");
 	}	
 	if (userCmd == "sus") {
-		//msgRef = msg.channel.send(":face_with_raised_eyebrow: :camera_with_flash:");
 		msg.channel.send(":face_with_raised_eyebrow: :camera_with_flash:").then((sentMessage) => {sleep(200); sentMessage.edit(":face_with_raised_eyebrow: :camera:")})
-		//sleep(100);
-		//msgRef.edit(":face_with_raised_eyebrow: :camera:");
 	}
 	/*
 	if (userCmd == "remind") {
 		const dailyTime = msg.content.slice(prefix.length).split()[1];
 	}*/
+	if (userCmd == "insult") {
+		//takes in parameters user, probability, and time. Time is when insulting stops.
+		params = msg.content.slice(prefix.length).split();
+		if (params.length > 4) {
+			msg.channel.send("Invalid number of parameters!");
+			return;
+		}
+		else {
+			user = params[1];
+			uid = int(user.substring(9,-1));
+			inslt = {
+				"user" : uid,
+				"prob" : params[2],
+				"time" : params[3]
+			}
+			data = JSON.stringify(inslt)
+			fs.writeFile("./insult.json", data)
+		}
+	}
 });
 client.login(config.DISCORD_TOKEN);
